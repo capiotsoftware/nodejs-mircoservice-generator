fa593a926094c4ea7cc26d35f58b435a26cf7d26
Shobhit Agarwal <shobhit@capiot.com>
2017-11-13 11:45:54 +0530

    option for sequelize and mongoose

--- projectSkeletons/templates/app.template.js
+++ projectSkeletons/templates/app.template.js
@@ -1,5 +1,5 @@
-module.exports = function(config){
-    var puttuRedis = config.puttuName?`const puttu = require("puttu-redis");
+module.exports = function (config) {
+    var puttuRedis = config.puttuName ? `const puttu = require("puttu-redis");
 const redis = require("redis");
 const client = redis.createClient();
 
@@ -10,13 +10,53 @@ client.on("error", function (err) {
 });
 
 puttu.init(client);
-    `:"";
-    var puttuRegister = config.puttuName?`puttu.register("${config.projectName}", {
+    `: "";
+    var puttuRegister = config.puttuName ? `puttu.register("${config.projectName}", {
         protocol: "http",
         port: port,
         api: "/${config.basePath}/"
     }).catch(err => logger.error(err));
-`:"";
+`: "";
+var dbCode = config.om === 'ODM' ? `const mongoose = require("mongoose");
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/${config.db}";
+global.Promise = bluebird;
+global.logger = logger;
+
+mongoose.connect(mongoUrl, err =>{
+    if(err){
+        logger.error(err);
+    }
+    else{
+        logger.info("Connected to DB");
+    }
+});`: `
+const Sequelize = require("sequelize");
+const pg = require('pg');
+var dbName = "${config.db}";
+dbName = dbName.toLowerCase();
+var conStringPostgres = 'postgres://postgres:welcome@localhost:5432/postgres';
+var conStringDB = 'postgres://postgres:welcome@localhost:5432/' + dbName;
+
+var pgClient = new pg.Client(conStringPostgres);
+try{
+pgClient.connect().then(()=>{
+    pgClient.query("select count(*) from pg_catalog.pg_database where datname = '"+dbName+"'").then(result=>{
+    var dbCount = result.rows[0].count;
+    if(dbCount=="1"){
+        console.log("Database Exist");
+        registerSwagger();
+    }else{
+        pgClient.query('CREATE DATABASE '+dbName).then(result=>{
+            registerSwagger();
+            });
+        }
+});
+
+});
+
+}catch(err){
+console.log(err);
+}`
     var appJs = `
 "use strict";
 const SwaggerExpress = require("swagger-express-mw");
@@ -25,37 +65,10 @@ const cuti = require("cuti");
 const log4js = cuti.logger.getLogger;
 const logger = log4js.getLogger("${config.projectName}");
 const bluebird = require("bluebird");
-const Sequelize = require("sequelize");
-const pg = require('pg');
+${dbCode}
 ${puttuRedis}
 global.Promise = bluebird;
 global.logger = logger;
-
-var dbName = "${config.db}";
-dbName = dbName.toLowerCase();
-var conStringPostgres = 'postgres://postgres:welcome@localhost:5432/postgres';
-var conStringDB = 'postgres://postgres:welcome@localhost:5432/' + dbName;
-
-var pgClient = new pg.Client(conStringPostgres);
-try{
-    pgClient.connect().then(()=>{
-        pgClient.query("select count(*) from pg_catalog.pg_database where datname = '"+dbName+"'").then(result=>{
-		var dbCount = result.rows[0].count;
-		if(dbCount=="1"){
-			console.log("Database Exist");
-			registerSwagger();
-		}else{
-            pgClient.query('CREATE DATABASE '+dbName).then(result=>{
-				registerSwagger();
-				});
-			}
-	});
-
-  });
-
-}catch(err){
-  console.log(err);
-}
 var counter = 0;
 var logMiddleware = (req, res, next) => {
     var reqId = counter++;
